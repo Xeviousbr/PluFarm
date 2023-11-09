3dd674f65f3c46f8333f1b63126b223976de835c
Xeviousbr <xeviousbr@gmail.com>
2023-11-09 02:39:49 -0300

    Rota compare_pdfs

--- PluFarm/runserver.py
+++ PluFarm/runserver.py
@@ -1,6 +1,16 @@
 from flask import Flask, request, jsonify, send_from_directory
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'pdf'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/upload_pdf', methods=['POST'])
 def upload_pdf():
@@ -22,6 +32,22 @@ def serve_ai_plugin():
 @app.route('/.well-known/openapi.yaml')
 def serve_openapi_yaml():
     return send_from_directory('.', 'openapi.yaml', mimetype='text/yaml')
+
+@app.route('/compare_pdfs', methods=['POST'])
+def compare_pdfs():
+    if 'pdf1' not in request.files or 'pdf2' not in request.files:
+        return jsonify(error="compare_pdfs")
+    file1 = request.files['pdf1']
+    file2 = request.files['pdf2']        
+    if file1.filename == '' or file2.filename == '':
+        return jsonify(error="Nenhum arquivo selecionado")
+    if file1 and allowed_file(file1.filename) and file2 and allowed_file(file2.filename):
+        filepath1 = os.path.join(app.config['UPLOAD_FOLDER'], file1.filename)
+        filepath2 = os.path.join(app.config['UPLOAD_FOLDER'], file2.filename)
+        file1.save(filepath1)
+        file2.save(filepath2)
+        return jsonify(success="compare_pdfs")
+    return jsonify(success="compare_pdfs")
 
 if __name__ == '__main__':
     app.run(debug=True)
